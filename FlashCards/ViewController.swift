f44c6229e895314dd2bff5652f85f2a6cbf059b5
BigO Argueta <bigo_0976@icloud.com>
2018-10-13 15:41:29 -0500

    Initial Commit

---
+++ FlashCards/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlashCards
+//
+//  Created by Oswaldo Argueta on 10/13/18.
+//  Copyright © 2018 Oswaldo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
